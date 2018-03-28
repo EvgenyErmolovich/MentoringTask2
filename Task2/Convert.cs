60741e60aff6e0d5d0aa5e1d4b351079dcfaf427
Evgeny <evga.1998@mail.ru>
2018-03-28 10:19:23 +0300

    Updated with RegularExpressions

--- Task2/Convert.cs
+++ Task2/Convert.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Task2
 {
@@ -6,31 +7,23 @@ namespace Task2
     {
         public static int ConvertToInt(string input)
         {
-            string output = "";
             int result = 0;
+            Regex regex = new Regex(@"\d");
             try
             {
-                for (int i = 0; i < input.Length; i++)
-                {
-                    if (input[i] < 'A' && input[i] != ' ') output += input[i];
-                }
-            }
-            catch (NullReferenceException exp)
-            {
-                Console.WriteLine(exp.Message);
-                return result;
-            }
-
-            try
-            {
-                if (output == "") throw new StringWithoutNumbersException("Your string doesn't contain numbers!");
-                int length = output.Length - 1;
+                MatchCollection matches = regex.Matches(input);         
+                int length = matches.Count - 1;
+                if (length < 0) throw new StringWithoutNumbersException("Your string doesn't contain numbers!");
                 for (int i = length; i >= 0; i--)
                 {
-                    result += (output[i] - '0') * (int)Math.Pow(10, length - i);
+                    result += (matches[i].Value[0] - '0') * (int)Math.Pow(10, length - i);
                 }
             }
-            catch(StringWithoutNumbersException exp)
+            catch (ArgumentNullException exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+            catch (StringWithoutNumbersException exp)
             {
                 Console.WriteLine(exp.Message);
             }
